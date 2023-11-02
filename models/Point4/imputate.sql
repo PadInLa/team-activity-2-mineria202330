dbe5407ce5ed317db26238fcfae5d5b4a04cc518
Juan David Padilla Paez <53383305+PadInLa@users.noreply.github.com>
2023-11-02 01:32:29 -0500

    Point4 Renamed variables for functional purposes

--- models/Point4/imputate.sql
+++ models/Point4/imputate.sql
@@ -1,4 +1,4 @@
 {{ config(materialized='table') }}
 
-SELECT * EXCEPT(Precio, median), IFNULL(Precio, median) AS Precio
-FROM {{ source("supermarket", "Olimpica") }}, {{ ref("median")}}
+SELECT * EXCEPT(Precio, median), IF NULL(Precio, median) AS Precio
+FROM {{ source("supermarket", "Olimpica") }}, {{ ref("median_")}}
