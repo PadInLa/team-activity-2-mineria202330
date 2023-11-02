486ba082851f281110cf018c6c77e09d36ffbc40
Juan David Padilla Paez <53383305+PadInLa@users.noreply.github.com>
2023-11-02 04:08:45 -0500

    Point4 Refactored for BigQuery usage

--- models/Point4/imputate.sql
+++ models/Point4/imputate.sql
@@ -1,30 +1,33 @@
 {{ config(materialized='table') }}
 
-WITH mean_price AS (
-    -- Total number of non-null Precio entries.
-    SELECT Precio, COUNT(*) OVER() AS cantidad
+WITH Olimpica_Prices AS (
+    SELECT Precio
     FROM {{ source("supermarket", "Olimpica") }}
     WHERE Precio IS NOT NULL
-    ORDER BY Precio
 ),
 
-add_row_id AS (
-    -- Selects all fields from model mean_price and assign a unique row number to each row.
-    SELECT *, ROW_NUMBER() OVER () AS row_id
-    FROM mean_price
-)
+Ranked_Prices AS (
+    SELECT 
+        Precio,
+        COUNT(*) OVER() AS total_count,
+        ROW_NUMBER() OVER(ORDER BY Precio) AS rn_asc,
+        ROW_NUMBER() OVER(ORDER BY Precio DESC) AS rn_desc
+    FROM Olimpica_Prices
+),
 
-median_ AS (
-    -- Calculates the median of the prices of the products in the table depending if the number of rows is even or odd.
-    SELECT
-    CASE
-    WHEN MOD(cantidad, 2)=0 THEN (SELECT AVG(Precio) FROM add_row_id WHERE row_id BETWEEN cantidad/2 AND cantidad/2+1)
-    ELSE (SELECT Precio FROM add_row_id WHERE row_id=ROUND(cantidad/2))
-    END median
-    FROM add_row_id
-    LIMIT 1
+Median_Price AS (
+    SELECT 
+        AVG(Precio) AS median
+    FROM Ranked_Prices
+    WHERE rn_asc BETWEEN total_count/2.0 AND total_count/2.0 + 1
+    OR rn_desc BETWEEN total_count/2.0 AND total_count/2.0 + 1
 )
 
 -- Creates imputate table with the median of the prices in case of null values
-SELECT * EXCEPT(Precio, median), IFNULL(Precio, median) AS Precio
-FROM {{ source("supermarket", "Olimpica") }}, median_
+SELECT 
+    Olimpica.* EXCEPT(Precio),
+    IFNULL(Olimpica.Precio, Median_Price.median) AS Precio
+FROM 
+    {{ source("supermarket", "Olimpica") }} AS Olimpica
+CROSS JOIN 
+    Median_Price
