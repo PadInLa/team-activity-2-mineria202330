0d46ebb622df243ddd8b9cb455261dcf17c2621f
Juan David Padilla Paez <53383305+PadInLa@users.noreply.github.com>
2023-11-02 01:20:59 -0500

    Point 4 Fixing incorrect references

--- models/Point4/imputate.sql
+++ models/Point4/imputate.sql
@@ -1,4 +1,4 @@
 {{ config(materialized='table') }}
 
 SELECT * EXCEPT(Precio, median), IFNULL(Precio, median) AS Precio
-FROM {{ source("supermarket", "Olimpica") }}, {{ ref("median_")}}
+FROM {{ source("supermarket", "Olimpica") }}, {{ ref("median")}}
