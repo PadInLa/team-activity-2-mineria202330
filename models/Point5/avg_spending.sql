a6fa09bc3d10cfd55a19984a551f4370bf66909e
Juan David Padilla Paez <53383305+PadInLa@users.noreply.github.com>
2023-11-02 01:59:36 -0500

    Point5 First query new test

--- models/Point5/avg_spending.sql
+++ models/Point5/avg_spending.sql
@@ -33,7 +33,7 @@ por_almacen as (
     vino_tinto_.Codigo AS producto, compras_.cantidad,
     compras_.cantidad * vino_tinto_.Precio AS total
     FROM Compras_Aggregated compras_ INNER JOIN Vinotinto_Pro vino_tinto_ ON compras_.producto = vino_tinto_.Codigo
-)
+),
 
 SELECT *,
 (SUM(total) OVER (particion_almacen)) / (SUM(cantidad) OVER (particion_almacen)) promedio_almacen
