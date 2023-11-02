952e387a4f90b04fe9befef545fea4544095c000
Juan David Padilla Paez <53383305+PadInLa@users.noreply.github.com>
2023-11-02 02:02:15 -0500

    Point5 First query retest

--- models/Point5/avg_spending.sql
+++ models/Point5/avg_spending.sql
@@ -28,16 +28,19 @@ Vinotinto_Pro AS (
     FROM Vinotinto_Olimpica
 )
 
-por_almacen as (
-    SELECT IF(SUBSTR(vino_tinto_.Codigo,1,3)='OLI', 'Olimpica', 'EXITO') AS almacen,
-    vino_tinto_.Codigo AS producto, compras_.cantidad,
-    compras_.cantidad * vino_tinto_.Precio AS total
-    FROM Compras_Aggregated compras_ INNER JOIN Vinotinto_Pro vino_tinto_ ON compras_.producto = vino_tinto_.Codigo
-),
-
-SELECT *,
-(SUM(total) OVER (particion_almacen)) / (SUM(cantidad) OVER (particion_almacen)) promedio_almacen
-FROM por_almacen
-WINDOW particion_almacen AS (
-  PARTITION BY almacen
-)
+SELECT 
+  almacen,
+  producto,
+  cantidad,
+  total,
+  (SUM(total) OVER (PARTITION BY almacen)) / (SUM(cantidad) OVER (PARTITION BY almacen)) AS promedio_almacen
+FROM (
+    SELECT 
+      IF(SUBSTR(vino_tinto_.Codigo, 1, 3) = 'OLI', 'Olimpica', 'EXITO') AS almacen,
+      vino_tinto_.Codigo AS producto, 
+      compras_.cantidad,
+      compras_.cantidad * vino_tinto_.Precio AS total
+    FROM Compras_Aggregated compras_
+    INNER JOIN Vinotinto_Pro vino_tinto_ 
+    ON compras_.producto = vino_tinto_.Codigo
+) por_almacen
