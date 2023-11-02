878d542efa015eee7b7efe986e00b25774218087
Juan David Padilla Paez <53383305+PadInLa@users.noreply.github.com>
2023-11-02 02:54:43 -0500

    Point5 Third query done, Fourth Query test

---
+++ models/Point5/popular_purchases.sql
@@ -0,0 +1,25 @@
+{{ config(materialized='table') }}
+
+-- Query that returns the most bought products from both supermarkets.
+SELECT 
+  allprod_.*,
+  COALESCE(purchases.Frecuencia, 0) AS Frecuencia
+FROM 
+  (
+    SELECT * 
+    FROM {{ source("supermarket", "Exito") }}
+
+    UNION ALL
+
+    SELECT * 
+    FROM {{ source("supermarket", "imputate") }}
+  ) allprod_
+LEFT JOIN 
+  (
+    SELECT producto, COUNT(producto) AS Frecuencia
+    FROM {{ source("supermarket", "Compras") }}
+    GROUP BY producto
+  ) purchases 
+ON allprod_.Codigo = purchases.producto
+ORDER BY 
+  Frecuencia DESC
